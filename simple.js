8dd1d22990679256e2072f338d8d85c714447296
cooper09 <cooper09@Biggie-II.local>
2014-12-15 09:14:58 -0500

    simple rest app

--- simple.js
+++ simple.js
@@ -32,10 +32,45 @@ mongoClient.open(function(err, mongoClient) { //C
   console.log("ready for our collection...");
 });
 
+/*  cooper s - for simple get testing
+	
 app.get('/', function (req, res) {
   res.send('<html><body><h1>Hulk Smash!!!</h1></body></html>');
 });
- 
+ */
+
+//set up Gets for each collection
+app.get('/:collection', function(req, res) { //A
+console.log("ta-da!!!", req.params);
+   var params = req.params; //B
+   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
+   		console.log("find all: ", req.params.collection);
+    	  if (error) { res.send(400, error); } //D
+	      else { 
+	          if (req.accepts('html')) { //E
+	          	console.log("receive collection objects: ");
+    	          res.render('data',{objects: objs, collection: req.params.collection}); //F
+              } else {
+	          res.set('Content-Type','application/json'); //G
+                  res.send(200, objs); //H
+              }
+         }
+   	});
+});
+
+app.get('/:collection/:entity', function(req, res) { //I
+   var params = req.params;
+   var entity = params.entity;
+   var collection = params.collection;
+   if (entity) {
+       collectionDriver.get(collection, entity, function(error, objs) { //J
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //K
+       });
+   } else {
+      res.send(400, {error: 'bad url', url: req.url});
+   }
+});
 
 // start server 
 http.createServer(app).listen(app.get('port'), function(){
